3e7bb2f7226834ba550512889c025b47fc237ca4
asolodova <angel.solodova@gmail.com>
2019-11-30 01:56:13 +0300

    Use DataProvider

--- src/test/java/qa/tests/tests/GroupCreationTest.java
+++ src/test/java/qa/tests/tests/GroupCreationTest.java
@@ -1,8 +1,13 @@
 package qa.tests.tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import qa.tests.model.GroupData;
 import qa.tests.model.Groups;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -10,12 +15,18 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class GroupCreationTest extends TestBase {
 
 
-  @Test
-  public void testGroupCreation() {
-
+  @DataProvider
+  public Iterator <Object[]> validGroups(){
+    List<Object[]> list = new ArrayList<Object[]>();
+    list.add(new Object[]{new GroupData().withName("test1").withHeader("header 01").withFooter("footer 01")});
+    list.add(new Object[]{new GroupData().withName("test2").withHeader("header 02").withFooter("footer 02")});
+    list.add(new Object[]{new GroupData().withName("test3").withHeader("header 03").withFooter("footer 03")});
+    return list.iterator();
+  }
+  @Test(dataProvider = "validGroups")
+  public void testGroupCreation(GroupData group) {
     app.goTo().groupPage();
     Groups before = app.group().all();
-    GroupData group = new GroupData().withName("Test2");
     app.group().create(group);
     assertThat(app.group().count(), equalTo(before.size() + 1));
     Groups after = app.group().all();
