8c225121eddabc7200a46e2e6c4a690677dd8598
asolodova <angel.solodova@gmail.com>
2019-12-01 04:29:14 +0300

    add hibernate for contactData

--- src/test/java/qa/tests/model/ContactData.java
+++ src/test/java/qa/tests/model/ContactData.java
@@ -1,35 +1,92 @@
 package qa.tests.model;
 
+import com.google.gson.annotations.Expose;
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+import com.thoughtworks.xstream.annotations.XStreamOmitField;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.*;
 import java.io.File;
 import java.util.Objects;
 
+@XStreamAlias("contact")
+@Entity
+@Table(name = "addressbook")
+
 public class ContactData {
+  @XStreamOmitField
+  @Id
+  @Column(name = "id")
   private int id = Integer.MAX_VALUE;
+  @Expose
+  @Column(name = "firstname")
   private String firstname;
+  @Expose
+  @Column(name = "lastname")
   private String lastname;
+  @Expose
+  @Column(name = "middlename")
   private String middlename;
+  @Expose
+  @Column(name = "nickname")
   private String nickname;
+  @Expose
+  @Column(name = "address")
+  @Type(type = "text")
   private String address;
+  @Expose
+  @Column(name = "email")
+  @Type(type = "text")
   private String email;
+  @Expose
+  @Column(name = "email2")
+  @Type(type = "text")
   private String email2;
+  @Expose
+  @Column(name = "email3")
+  @Type(type = "text")
   private String email3;
+  @Expose
+  @Transient
   private String allEmails;
+  @Expose
+  @Column(name = "home")
+  @Type(type = "text")
   private String homePhone;
+  @Expose
+  @Column(name = "mobile")
+  @Type(type = "text")
   private String mobilePhone;
+  @Expose
+  @Column(name = "work")
+  @Type(type = "text")
   private String workPhone;
+  @Expose
+  @Transient
   private String allPhones;
+  @Expose
+  @Column(name = "title")
   private String title;
+  @Expose
+  @Column(name = "company")
   private String company;
+  @Expose
+  @Transient
   private String group;
+  @Expose
+  @Transient
   private String allData;
-  private File photo;
+  @Expose
+  @Column(name = "photo")
+  @Type(type = "text")
+  private String  photo;
 
   public File getPhoto() {
-    return photo;
+    return new File(photo);
   }
 
   public ContactData withPhoto(File photo) {
-    this.photo = photo;
+    this.photo = photo.getPath();
     return this;
   }
 
