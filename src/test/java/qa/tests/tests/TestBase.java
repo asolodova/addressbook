51024135a6ed2c5ffa041fd9309434612ee835ba
asolodova <angel.solodova@gmail.com>
2019-12-01 05:12:28 +0300

    add parameter launch

--- src/test/java/qa/tests/tests/TestBase.java
+++ src/test/java/qa/tests/tests/TestBase.java
@@ -8,9 +8,15 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import qa.tests.appmanager.ApplicationManager;
+import qa.tests.model.GroupData;
+import qa.tests.model.Groups;
 
 import java.lang.reflect.Method;
 import java.util.Arrays;
+import java.util.stream.Collectors;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 /**
  * Created by Lina on 25.03.2017.
@@ -39,5 +45,14 @@ public class TestBase {
   public void logTestStop(Method m){
     logger.info("Stop test " + m.getName());
 
+  }
+  public void verifyGroupListInUI() {
+    if (Boolean.getBoolean("verifyUI")){
+      Groups dbGroups = app.db().groups();
+      Groups uiGroups = app.group().all();
+      assertThat(uiGroups, equalTo(dbGroups.stream()
+              .map((g) -> new GroupData().withId(g.getId()).withName(g.getName()))
+              .collect(Collectors.toSet())));
+    }
   }
 }
